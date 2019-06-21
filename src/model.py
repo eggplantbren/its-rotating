114db1c3bf8e5a4d06860bcf47c68ad3785d2335
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-06-21 13:24:00 +1200

    A very basic initial model

--- src/model.py
+++ src/model.py
@@ -1,26 +1,28 @@
 import numpy as np
 import dnest4.builder as bd
 
-data = {"x": np.array([1.0, 2.0, 3.0, 4.0, 5.0]),\
-        "y": np.array([1.0, 2.0, 3.0, 3.9, 5.1]),\
+data = {"x": np.array([1.0, 2.0, 3.0, 4.0, 5.0]),
+        "y": np.array([-1.0, 2.0, -3.0, 3.9, 5.1]),
+        "v": np.array([20.0, -40.0, 23.3, 39.2, -11.2]),
+        "sig_v": np.array([1.0, 2.0, 0.5, 2.2, 1.2]),
         "N": 5}
 
 # Create the model
 model = bd.Model()
 
-# Slope and intercept
-model.add_node(bd.Node("m", bd.Uniform(-100.0, 100.0)))
-model.add_node(bd.Node("b", bd.Uniform(-100.0, 100.0)))
-
-# Noise standard deviation
-model.add_node(bd.Node("log_sigma", bd.Uniform(-10.0, 10.0)))
-model.add_node(bd.Node("sigma", bd.Delta("exp(log_sigma)")))
+# Velocity dispersion
+model.add_node(bd.Node("log_velocity_dispersion", bd.Normal(0.0, 10.0)))
+model.add_node(bd.Node("velocity_dispersion",
+                            bd.Delta("exp(log_velocity_dispersion)")))
 
 # p(data | parameters)
 for i in range(0, data["N"]):
-    name = "y{index}".format(index=i)
-    mean = "m*x{index} + b".format(index=i)
-    model.add_node(bd.Node(name, bd.Normal(mean, "sigma"), observed=True))
+    # This will be a bit slow but it doesn't matter
+    stdev = "sqrt(pow(sig_v{index}, 2) + pow(velocity_dispersion, 2))"\
+                .format(index=i)
+
+    name = "v{index}".format(index=i)
+    model.add_node(bd.Node(name, bd.Normal(0.0, stdev), observed=True))
 
 # Create the C++ code
 bd.generate_h(model, data)
