46337decfa233e9b3dc4fd297b6ecaef58f636df
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-06-21 14:45:26 +1200

    More stuff

--- src/model.py
+++ src/model.py
@@ -9,6 +9,10 @@ data = { "x": raw[:,0],
          "sig_v": raw[:,3],
          "N": raw.shape[0] }
 
+# Polar coordinates
+data["r"] = np.sqrt(data["x"]**2 + data["y"]**2)
+data["theta"] = np.arctan2(data["y"], data["x"])
+
 # Create the model
 model = bd.Model()
 
@@ -18,17 +22,30 @@ model.add_node(bd.Node("velocity_dispersion",
                             bd.Delta("exp(log_velocity_dispersion)")))
 
 # Constant velocity offset
-model.add_node(bd.Node("c", bd.T(0.0, 0.1, 1.0)))
-model.add_node(bd.Node("mu_v", bd.Delta("c*velocity_dispersion")))
+model.add_node(bd.Node("c_v_systematic", bd.T(0.0, 0.1, 1.0)))
+model.add_node(bd.Node("v_systematic",
+                    bd.Delta("c_v_systematic*velocity_dispersion")))
+
+# Rotation amplitude
+model.add_node(bd.Node("log_c_A", bd.T(-2.0, 1.0, 1.0)))
+model.add_node(bd.Node("c_A", bd.Delta("exp(log_c_A)")))
+model.add_node(bd.Node("A", bd.Delta("c_A*velocity_dispersion")))
+
+# Rotation angle
+model.add_node(bd.Node("phi", bd.Uniform(0.0, 2.0*np.pi)))
 
 # p(data | parameters)
 for i in range(0, data["N"]):
+
+    # 
+    mean = "v_systematic + A*sin(theta{index} - phi)".format(index=i)
+
     # This will be a bit slow but it doesn't matter
     stdev = "sqrt(pow(sig_v{index}, 2) + pow(velocity_dispersion, 2))"\
                 .format(index=i)
 
     name = "v{index}".format(index=i)
-    model.add_node(bd.Node(name, bd.Normal("mu_v", stdev), observed=True))
+    model.add_node(bd.Node(name, bd.Normal(mean, stdev), observed=True))
 
 # Create the C++ code
 bd.generate_h(model, data)
