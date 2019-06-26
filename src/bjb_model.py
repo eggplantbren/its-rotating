b8cbec75a0b7cffc9cc341fdcfa102fa4db81cd9
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-06-26 13:02:29 +1200

    Updated my priors

--- src/bjb_model.py
+++ src/bjb_model.py
@@ -15,9 +15,9 @@ data = { "x": raw[:,0],
 model = bd.Model()
 
 # Constant velocity dispersion
-model.add_node(bd.Node("log_velocity_dispersion", bd.T(4.605, 2.0, 1.0)))
+model.add_node(bd.Node("log10_velocity_dispersion", bd.T(2.0, 1.0, 2.0)))
 model.add_node(bd.Node("velocity_dispersion",
-                            bd.Delta("exp(log_velocity_dispersion)")))
+                            bd.Delta("pow(10.0, log10_velocity_dispersion)")))
 
 # Constant velocity offset
 model.add_node(bd.Node("c_v_systematic", bd.T(0.0, 0.1, 1.0)))
@@ -25,9 +25,9 @@ model.add_node(bd.Node("v_systematic",
                     bd.Delta("c_v_systematic*velocity_dispersion")))
 
 # Rotation amplitude
-model.add_node(bd.Node("log_c_A", bd.T(-2.0, 1.0, 1.0)))
-model.add_node(bd.Node("c_A", bd.Delta("exp(log_c_A)")))
-model.add_node(bd.Node("A", bd.Delta("c_A*velocity_dispersion")))
+model.add_node(bd.Node("t_A", bd.T(0.0, 2.0, 2.0)))
+model.add_node(bd.Node("A", bd.Delta("pow(10.0, 1.0-\
+                                       std::abs(t_A))*velocity_dispersion")))
 
 # Rotation angle
 model.add_node(bd.Node("phi", bd.Uniform(0.0, 2.0*np.pi)))
