422b13a3ed93896c9ced0ea853f5e1cf207d3bdb
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-06-26 14:23:31 +1200

    Alternate plot for the bjb priors

--- src/showresults.py
+++ src/showresults.py
@@ -1,15 +1,18 @@
+import corner
 import dnest4.classic as dn4
 import matplotlib.pyplot as plt
-import corner
+import numpy as np
+
 dn4.postprocess()
 
 # Fonts
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["font.size"] = 14
 plt.rc("text", usetex=True)
-#plt.rcParams["axes.labelpad"] = 24
 
 posterior_sample = dn4.my_loadtxt("posterior_sample.txt")
+
+
 fig = corner.corner(posterior_sample,
               labels=["$\\Sigma$ (km/s)", "$v_{\\rm sys}$ (km/s)",
                         "$A$ (km/s)", "$\phi$ (rad.)"],
@@ -18,4 +21,14 @@ fig = corner.corner(posterior_sample,
               label_kwargs=dict(fontsize=18))
 plt.savefig("corner_rotation_gfl_priors.png", dpi=300)
 plt.show()
+
+## Version for the BJB priors
+#fig = corner.corner(posterior_sample[:, np.array([1, 3, 5, 6])],
+#              labels=["$\\Sigma$ (km/s)", "$v_{\\rm sys}$ (km/s)",
+#                        "$A$ (km/s)", "$\phi$ (rad.)"],
+#              plot_contours=False, plot_density=False,
+#              data_kwargs={"alpha": 0.15, "ms": 0.75},
+#              label_kwargs=dict(fontsize=18))
+#plt.savefig("corner_rotation_bjb_priors.png", dpi=300)
+#plt.show()
 
